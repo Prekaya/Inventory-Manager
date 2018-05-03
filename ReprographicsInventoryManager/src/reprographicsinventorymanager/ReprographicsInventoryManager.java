fbc89ea01e2b44f74a7c827073a6b852f5fa6b01
Femi Lamptey <femi.lamptey@gmail.com>
2018-05-03 13:09:18 +0000

    Added sign in code
    No credentials required to sign in, just click the sign in button. If required, change the code a bit to test out the screen you desire to test.

--- ReprographicsInventoryManager/src/reprographicsinventorymanager/ReprographicsInventoryManager.java
+++ ReprographicsInventoryManager/src/reprographicsinventorymanager/ReprographicsInventoryManager.java
@@ -1,24 +1,40 @@
 package reprographicsinventorymanager;
 
+import java.net.URL;
 import java.sql.SQLException;
 import javafx.application.Application;
+import javafx.beans.property.Property;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import reprographicsinventorymanager.ScreenControllers.LoginScreenController;
 
 /**
  *
  * @author ADDO_a
  */
 public class ReprographicsInventoryManager extends Application {
-    
+
     @Override
-    public void start(Stage primaryStage) throws SQLException {
-        
-        Button btn = new Button();
+    public void start(Stage primaryStage) throws Exception {
+
+        AnchorPane root = FXMLLoader.load(getClass().getResource("Screens/LoginScreen.fxml"));
+
+        Scene scene = new Scene(root, 355, 500);
+
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
+        primaryStage.show();
+
+        /* Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
@@ -35,7 +51,8 @@ public class ReprographicsInventoryManager extends Application {
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
-        primaryStage.show();
+        primaryStage.show(); */
+
     }
 
     /**
--- ReprographicsInventoryManager/src/reprographicsinventorymanager/ScreenControllers/LoginScreenController.java
+++ ReprographicsInventoryManager/src/reprographicsinventorymanager/ScreenControllers/LoginScreenController.java
@@ -7,7 +7,15 @@ package reprographicsinventorymanager.ScreenControllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import reprographicsinventorymanager.ReprographicsInventoryManager;
 
 /**
  * FXML Controller class
@@ -19,9 +27,17 @@ public class LoginScreenController implements Initializable {
     /**
      * Initializes the controller class.
      */
+
+    @FXML private AnchorPane pane;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    public void signIn() throws Exception {
+        Pane root = FXMLLoader.load(getClass().getResource("../Screens/ViewRequestScreen.fxml"));
+        pane.getChildren().setAll(root);
+    }
+
 }
